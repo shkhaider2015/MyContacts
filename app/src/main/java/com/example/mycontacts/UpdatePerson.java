95f444dc90767d7b2a48f671ed1fa534e4440b73
Haider <shkhaider2015@gmail.com>
2020-02-22 03:44:07 +0500

    Update Complete

--- app/src/main/java/com/example/mycontacts/UpdatePerson.java
+++ app/src/main/java/com/example/mycontacts/UpdatePerson.java
@@ -1,17 +1,30 @@
 package com.example.mycontacts;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
-public class UpdatePerson extends AppCompatActivity implements View.OnClickListener {
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class UpdatePerson extends AppCompatActivity {
+
+    private static final int updateImageChooser = 102;
+    private static final String TAG = "UpdatePerson";
 
     private ImageView imageView;
 
@@ -28,14 +41,38 @@ public class UpdatePerson extends AppCompatActivity implements View.OnClickListe
 
     private Button mUpdate;
 
+
+    private byte[] imageBytes;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_person);
         init();
 
         final Contacts contact = (Contacts) getIntent().getSerializableExtra("update_contact");
         loadInfo(contact);
+
+        imageView.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent imgIntent = new Intent();
+                imgIntent.setType("image/*");
+                imgIntent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(imgIntent, "Select Profile Picture"), updateImageChooser);
+            }
+        });
+        mUpdate.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                updateInfo(contact);
+            }
+        });
     }
 
     private void init()
@@ -49,23 +86,16 @@ public class UpdatePerson extends AppCompatActivity implements View.OnClickListe
         mFamily = findViewById(R.id.activity_update_family);
         mClassmate = findViewById(R.id.activity_update_classmate);
         mUpdate = findViewById(R.id.activity_update_update);
+
+
+        imageBytes = null;
     }
 
-    @Override
-    public void onClick(View v)
-    {
-        switch (v.getId())
-        {
-            case R.id.activity_update_update:
-
-                break;
-        }
-
-    }
 
     private void loadInfo(Contacts contacts)
     {
-        Bitmap bitmap = ImageUtility.getImage(contacts.getImagePath());
+        imageBytes = contacts.getImagePath();
+        Bitmap bitmap = ImageUtility.getImage(imageBytes);
         if (bitmap != null)
         {
             imageView.setImageBitmap(bitmap);
@@ -87,4 +117,99 @@ public class UpdatePerson extends AppCompatActivity implements View.OnClickListe
             mClassmate.setChecked(true);
 
     }
+
+    private void updateInfo(Contacts contacts)
+    {
+        String name = mFullName.getText().toString().trim();
+        String number = mPhoneNumber.getText().toString().trim();
+        String email = mEmail.getText().toString().trim();
+        String category = getCategory();
+
+
+
+        class UpdateInformation extends AsyncTask<Void, Void, Void>
+        {
+
+            @Override
+            protected Void doInBackground(Void... voids) {
+                DatabaseClint
+                        .getInstance(UpdatePerson.this)
+                        .getAppDatabase()
+                        .contactsDao()
+                        .update(contacts);
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                super.onPostExecute(aVoid);
+
+                Toast.makeText(getApplicationContext(), "Updated", Toast.LENGTH_SHORT)
+                        .show();
+
+                startActivity(new Intent(UpdatePerson.this, MainActivity.class));
+            }
+        }
+
+        if(name.isEmpty())
+        {
+            mFullName.setError("Name is required");
+            mFullName.requestFocus();
+            return;
+        }
+        if (number.isEmpty())
+        {
+            mPhoneNumber.setError("Number is required");
+            mPhoneNumber.requestFocus();
+            return;
+        }
+
+        contacts.setFullName(name);
+        contacts.setPhoneNumber(number);
+        contacts.setEmail(email);
+        contacts.setCategory(category);
+        contacts.setImagePath(imageBytes);
+
+        UpdateInformation updateInformation = new UpdateInformation();
+        updateInformation.execute();
+    }
+
+    private String getCategory()
+    {
+        String checked = null;
+        switch (mRadioGroup.getCheckedRadioButtonId())
+        {
+            case R.id.activity_update_friend:
+                checked = "Friend";
+                break;
+            case R.id.activity_update_family:
+                checked = "Family";
+            case R.id.activity_update_classmate:
+                checked = "Classmate";
+        }
+
+        return checked;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+        Uri selectedPic = null;
+        if (requestCode == updateImageChooser && resultCode == RESULT_OK && data != null && data.getData() != null)
+        {
+            selectedPic = data.getData();
+
+            imageView.setImageURI(selectedPic);
+
+            try {
+                imageBytes = ImageUtility.getBytes(getContentResolver().openInputStream(selectedPic));
+            }catch (IOException e)
+            {
+                Log.e(TAG, "onActivityResult: ", e);
+            }
+        }
+    }
+
+
 }
