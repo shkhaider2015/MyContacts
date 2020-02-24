2835accd530aa8ad7dd9831c94c477b37807ac6b
Haider <shkhaider2015@gmail.com>
2020-02-24 09:57:21 +0500

    icons

--- app/src/main/java/com/example/mycontacts/MainActivity.java
+++ app/src/main/java/com/example/mycontacts/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -27,6 +28,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         init();
 
         mAdd.setOnClickListener(this::onClick);
