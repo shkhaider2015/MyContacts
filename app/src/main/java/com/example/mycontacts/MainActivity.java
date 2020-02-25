9f0a24b029864428eb48545ab2ab4c5ec8e4adec
Haider <shkhaider2015@gmail.com>
2020-02-25 18:30:46 +0500

    almost done

--- app/src/main/java/com/example/mycontacts/MainActivity.java
+++ app/src/main/java/com/example/mycontacts/MainActivity.java
@@ -13,7 +13,10 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Spinner;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.List;
 
@@ -24,7 +27,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private static final String TAG = "MainActivity";
 
 
-    private Button mAdd;
+    private ImageButton mAdd;
     private RecyclerView mRecyclerView;
     private Spinner mSpinner;
 
